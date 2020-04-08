69774567b3ad340389fed09bdf41a8f20b9994b4
hrrs01 <haavard.hn@gmail.com>
2020-04-08 17:57:11 +0200

    made the login system acctually secure lol

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Ignore this mess
 const express = require('express')
 const next = require('next')
 const DiscordOauth2 = require("discord-oauth2");
@@ -7,29 +8,38 @@ const handle = app.getRequestHandler()
 const port = 3000
 const session = require('express-session')
 
-
+// Discord oauth variables
 const oauth = new DiscordOauth2();
 const client_id = "697087272050229339"
 const client_secret = "JvzF9Eu_mvqCZRM0MPXrVsRh-T6V239R"
 const scope = "indetify"
 const redirect_uri = "http://localhost:3000/login"
 
-
+// Lets next do its thing before doing server stuff
 app.prepare().then(() =>{
+
+  // Initializes express server
   const server = express()
 
+  // the config for how the session variables are setup/stored
   sess_config = {
       secret: "legit good templates",
       cookie: {}
   }
+  // cuz why not
   if (process.env.NODE_ENV === 'production'){
     sess_config.cookie.secure = true;
   }
+  // make the server use the session
   server.use(session(sess_config))
 
+  // Here all parsing handled by the server goes
+
+  // Discord Oauth2 handeled on ./login
   server.get('/login', (req,res) => {
+    // isset(req.query.code)
     if(typeof req.query.code !== 'undefined'){
-      oauth.tokenRequest({
+      var token = oauth.tokenRequest({
         clientId: client_id,
         clientSecret: client_secret,
         code: req.query.code,
@@ -37,22 +47,41 @@ app.prepare().then(() =>{
         grantType: "authorization_code",
         redirectUri: redirect_uri
 
-      }).then(console.log("yup"))
-      req.session.loggedin = true;
+      }).then((result)=>{
+          if(typeof result.access_token !== 'undefined'){
+            console.log(result);
+            req.session.loggedin = true;
+            res.redirect("/");
+          }else{
+            res.send("That didnt work. Please contact support and we will look into it.");
+          }
+
+      })
+    }else{
+      if (req.session.loggedin !== true){
+        res.redirect("https://discordapp.com/api/oauth2/authorize?client_id=697087272050229339&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify");
+      }else{
+      // if logged in, why are they here
+        res.redirect("/");
+      }
     }
 
-    if (req.session.loggedin == null){
-      res.redirect("https://discordapp.com/api/oauth2/authorize?client_id=697087272050229339&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify");
-    }else{
-      res.redirect("/");
-    }
+
+
+
+    // if not logged in, redirect to discords auth page
+
 
 
   })
 
+  // let next handle the rest, to make it easy for the rest of you
   server.get('*', (req, res) => {
     return handle(req,res);
   })
+
+
+  // host the server
   server.listen(port, (err) => {
     if(err) throw err
     console.log("Hosting on localhost:3000")
